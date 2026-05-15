cdbdf67131c4a3e775bda04c0808ed617dbc0f8a
Gowtham-Bioinfo <dileepgowtham1947@gmail.com>
2026-05-15 13:06:58 +0530

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,8 @@
+## Day 8  : 30 Days of python programming
+
+## Exercises: Level 1
 
 
-
-Exercises: Level 1
 Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
 
 Enter your age: 30
