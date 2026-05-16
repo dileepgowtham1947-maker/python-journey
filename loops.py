16ab37a78174b8516d24b7a1742af298f37e95f8
Gowtham-Bioinfo <dileepgowtham1947@gmail.com>
2026-05-16 20:10:00 +0530

    Add files via upload

--- loops.py
+++ loops.py
@@ -1,54 +1,9 @@
-## Day 8  : 30 Days of python programming
+# Day 10 : 30 Days of python programming | start =  8:11 pm| End  =  pm
 
-## Exercises: Level 1
-
-
-Iterate 0 to 10 using for loop, do the same using while loop.
-
-Iterate 10 to 0 using for loop, do the same using while loop.
-
-Write a loop that makes seven calls to print(), so we get on the output the following triangle:
-
-  #
-  ##
-  ###
-  ####
-  #####
-  ######
-  #######
-Use nested loops to create the following:
-
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-# # # # # # # #
-Print the following pattern:
-
-0 x 0 = 0
-1 x 1 = 1
-2 x 2 = 4
-3 x 3 = 9
-4 x 4 = 16
-5 x 5 = 25
-6 x 6 = 36
-7 x 7 = 49
-8 x 8 = 64
-9 x 9 = 81
-10 x 10 = 100
-Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
-
-Use for loop to iterate from 0 to 100 and print only even numbers
-
-Use for loop to iterate from 0 to 100 and print only odd numbers
-
-
-
-
-
-## MY CODE FOR ALL PROBLEMS IN EX 1 :
-
-
+count = 10
+for i in range (0, 11) :
+    print(i)
+i = 0    
+while i <= 10 :
+    print(i)
+    i += 1
